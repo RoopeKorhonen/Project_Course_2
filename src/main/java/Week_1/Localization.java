a5c7e83d40e04d7ae65e72c044f5a2dd4b6c85f9
RoopeKorhonen <roope.korhonen2@metropolia.fi>
2024-03-27 10:48:58 +0200

    Init

---
+++ src/main/java/Week_1/Localization.java
@@ -0,0 +1,15 @@
+package Week_1;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Localization {
+    public static void main(String[] args) {
+        String lan= "fa";
+        String country = "IR";
+
+        Locale local = new Locale(lan, country);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", local);
+        System.out.println(resourceBundle.getString("greetings"));
+    }
+}
